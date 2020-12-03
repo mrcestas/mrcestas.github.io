fb91bc2effa4e7ed4d51c7e54d30dd16d87441b9
name <email>
2020-12-03 12:34:23 -0300

    base

---
+++ precache-manifest.e4258c8bccb4cd6bd0cd1a31b718d13f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e16568325ec74ff7e57f46edc30a0ff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "750b69cc8b4b165d95b2",
+    "url": "./static/css/2.91d7c233.chunk.css"
+  },
+  {
+    "revision": "722540beb7289960f262",
+    "url": "./static/css/main.e7e38ea1.chunk.css"
+  },
+  {
+    "revision": "750b69cc8b4b165d95b2",
+    "url": "./static/js/2.87237f44.chunk.js"
+  },
+  {
+    "revision": "6df4548ecb01cd88e57d1234281566da",
+    "url": "./static/js/2.87237f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "722540beb7289960f262",
+    "url": "./static/js/main.19b50dde.chunk.js"
+  },
+  {
+    "revision": "e736dabb8ee024a7b980",
+    "url": "./static/js/runtime-main.3993e964.js"
+  },
+  {
+    "revision": "179c25c200e22b0634c9e8441b9f1052",
+    "url": "./static/media/1.179c25c2.png"
+  },
+  {
+    "revision": "9eb5b18ee0fb434f6bb85f43d8165764",
+    "url": "./static/media/3.9eb5b18e.png"
+  },
+  {
+    "revision": "b8194be09c008cf672627c66c342b753",
+    "url": "./static/media/4.b8194be0.png"
+  },
+  {
+    "revision": "02ea7ce20f60742846b6d5c91090a72f",
+    "url": "./static/media/5.02ea7ce2.png"
+  },
+  {
+    "revision": "b79af9e834d7405794cad26d8106b0e0",
+    "url": "./static/media/Saira-Bold.b79af9e8.ttf"
+  },
+  {
+    "revision": "fb67c607c8cf801ca268bc738103ba7e",
+    "url": "./static/media/Saira-Regular.fb67c607.ttf"
+  },
+  {
+    "revision": "a2a9a20f7c5f2f2c4cb934ebde809794",
+    "url": "./static/media/grocery-cart-with-item-1005638.a2a9a20f.jpg"
+  },
+  {
+    "revision": "282235a9ee10782a9e1630bdeedfd3f3",
+    "url": "./static/media/labeled-can-lot-on-shelves-2387343.282235a9.jpg"
+  },
+  {
+    "revision": "4479eb5fef01fd58404c783a9c11af12",
+    "url": "./static/media/logoWhite.4479eb5f.png"
+  },
+  {
+    "revision": "4ade5e9e661d1504f27afcc71955964d",
+    "url": "./static/media/logo_round_alpha.4ade5e9e.png"
+  },
+  {
+    "revision": "3dbb68a1ab9ee8efb82bfe922b4e864e",
+    "url": "./static/media/street-market-fruits-pineapple-shopping-3326215.3dbb68a1.jpg"
+  }
+]);
